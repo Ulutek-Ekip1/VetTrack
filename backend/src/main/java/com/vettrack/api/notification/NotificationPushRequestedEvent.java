c08d33aff369a00d2e9a1444653880394cc43790
betulbilici <bet.ulbilici214@gmail.com>
2026-08-12 16:51:46 +0300

    Revert "dispatch visit notifications after transaction commit"
    This reverts commit 5dba0912f887b59082fb2034d56dc5781129ea7f.

--- backend/src/main/java/com/vettrack/api/notification/NotificationPushRequestedEvent.java
+++
@@ -1,6 +0,0 @@
-package com.vettrack.api.notification;
-
-import java.util.UUID;
-
-/** Published with the transaction that persists a notification. */
-public record NotificationPushRequestedEvent(UUID notificationId) {}
